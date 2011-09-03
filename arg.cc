ece618bb29d09d889ad5f3e77da801f12a5a110b
Forrest Y. Yu <forrest.yu@gmail.com>
2011-09-03 19:31:02 +0800

    trying to use Google's cpplint.py
    --HG--
    branch : src

--- arg.cc
+++ arg.cc
@@ -1,19 +1,34 @@
+// Copyright (c) 2011 Forrest Y. Yu. All rights reserved.
+
+// no error checked by Google's cpplint.py:
+//     $ cpplint.py arg.cc
+//     Done processing arg.cc
+//     Total errors found: 0
+
+// #include <iostream>  <-- cpplint.py doesn't like streams
+#include <cstdio>
 #include <cstdlib>
-#include <iostream>
+#include <string>
 #include <vector>
 
-int main(int argc, char * argv[])
-{
-	typedef std::vector<std::string> strvec;
-	strvec params(argv, argv + argc);
+int main(int argc, char * argv[]) {
+        typedef std::vector<std::string> strvec;
+        strvec params(argv, argv + argc);
 
-	for (strvec::const_iterator it = params.begin(); it != params.end(); ++it)
-		std::cout << *it << "+";
-	std::cout << std::endl;
+        for (strvec::const_iterator it = params.begin();
+             it != params.end(); ++it) {
+                // std::cout << *it << "+";
+                printf("%s+", it->c_str());
+        }
+        // std::cout << std::endl;
+        printf("\n");
 
-	for (int i = 1; i < argc; ++i)
-		std::cout << params[i] << "#";
-	std::cout << std::endl;
+        for (int i = 1; i < argc; ++i) {
+                // std::cout << params[i] << "#";
+                printf("%s#", params[i].c_str());
+        }
+        // std::cout << std::endl;
+        printf("\n");
 
-	return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
