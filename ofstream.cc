c0934fab5be4a75b417883986be3112c5cad227a
Forrest Y. Yu <forrest.yu@gmail.com>
2011-10-30 10:42:37 +0800

    learning C++ : std::ofstream
    --HG--
    branch : src

---
+++ ofstream.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char * argv[])
+{
+	if (argc != 2) {
+		std::cerr << argv[0] << " filename\n";
+		return 1;
+	}
+
+	std::ofstream ofile(argv[1], std::ios::app);
+	if (ofile.is_open())
+		ofile << "foo bar baz" << std::endl;
+	else
+		std::cerr << "Failed to open file.\n";
+
+	std::cout << "Done.\n";
+
+	return 0;
+}
