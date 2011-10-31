1e9049f6af6b586f9fd1d3f14c09286528c22349
Forrest Y. Yu <forrest.yu@gmail.com>
2011-10-31 23:41:32 +0800

    reformatted code
    --HG--
    branch : src

--- ifstream.cc
+++ ifstream.cc
@@ -4,69 +4,69 @@
 
 void rewind(std::ifstream& in)
 {
-        in.clear();
-        in.seekg(0, std::ios_base::beg);
+    in.clear();
+    in.seekg(0, std::ios_base::beg);
 }
 
 int main(int argc, char * argv[])
 {
-        if (argc < 2) {
-                fprintf(stderr, "usage: %s filename\n", argv[0]);
-                return EXIT_FAILURE;
-        }
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
 	std::ifstream ifile(argv[1]);
-        if (!ifile) {
-                perror("failed to open file");
-                return EXIT_FAILURE;
-        }
+    if (!ifile) {
+        perror("failed to open file");
+        return EXIT_FAILURE;
+    }
 
-        // read by line
-        // http://rosettacode.org/wiki/Read_a_file_line_by_line#C.2B.2B
+    // read by line
+    // http://rosettacode.org/wiki/Read_a_file_line_by_line#C.2B.2B
 	std::string line;
-        int i = 0;
-        // about ``getline()'':
-        // - http://en.cppreference.com/w/cpp/string/getline
-        // - http://augustcouncil.com/~tgibson/tutorial/iotips.html
+    int i = 0;
+    // about ``getline()'':
+    // - http://en.cppreference.com/w/cpp/string/getline
+    // - http://augustcouncil.com/~tgibson/tutorial/iotips.html
 	while (std::getline(ifile, line)) {
-                printf("%3d : %s\n", ++i, line.c_str());
+        printf("%3d : %s\n", ++i, line.c_str());
 	}
 
-        // rewind
-        rewind(ifile);
+    // rewind
+    rewind(ifile);
 
-        // read by word
-        std::string word;
-        while (ifile >> word)
-                printf(" + %s", word.c_str());
-        printf("\n");
+    // read by word
+    std::string word;
+    while (ifile >> word)
+        printf(" + %s", word.c_str());
+    printf("\n");
 
-        // rewind
-        rewind(ifile);
+    // rewind
+    rewind(ifile);
 
-        // read by int
-        //ifile.exceptions( std::ifstream::eofbit | std::ifstream::failbit | std::ifstream::badbit);
-        int num;
-        while (!ifile.eof()) {
-                ifile >> num;
-                if (ifile.rdstate() & std::ifstream::failbit) {
-                        ifile.clear();
-                        ifile >> word;
-                        printf("! this is not a number: %s\n", word.c_str());
-                } else {
-                        printf("got %3d\n", num);
-                }
+    // read by int
+    //ifile.exceptions( std::ifstream::eofbit | std::ifstream::failbit | std::ifstream::badbit);
+    int num;
+    while (!ifile.eof()) {
+        ifile >> num;
+        if (ifile.rdstate() & std::ifstream::failbit) {
+            ifile.clear();
+            ifile >> word;
+            printf("! this is not a number: %s\n", word.c_str());
+        } else {
+            printf("got %3d\n", num);
         }
+    }
 
-        // rewind
-        rewind(ifile);
+    // rewind
+    rewind(ifile);
 
-        // read entire file as string
-        // http://rosettacode.org/wiki/Read_entire_file#C.2B.2B
-        std::istreambuf_iterator<char> ifbegin(ifile);
-        std::istreambuf_iterator<char> ifend;
-        std::string fileData(ifbegin, ifend);
-        printf("\n--------\n%s\n--------\n", fileData.c_str());
+    // read entire file as string
+    // http://rosettacode.org/wiki/Read_entire_file#C.2B.2B
+    std::istreambuf_iterator<char> ifbegin(ifile);
+    std::istreambuf_iterator<char> ifend;
+    std::string fileData(ifbegin, ifend);
+    printf("\n--------\n%s\n--------\n", fileData.c_str());
 
 	ifile.close();
 
