5fa12c55167e9864fabb2f3d7210c918584db6f9
y² <yy@yuyuan.org>
2019-03-13 11:50:09 -0700

    [clion] MENU > File > New Project... > C++ Executable | C++17

---
+++ hi-clion/project_cpp_exe_helloworld/cmake-build-debug/CMakeFiles/project_cpp_exe_helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_cpp_exe_helloworld.dir/main.cpp.o"
+  "project_cpp_exe_helloworld.pdb"
+  "project_cpp_exe_helloworld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_cpp_exe_helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hi-clion/project_cpp_exe_helloworld/cmake-build-debug/CMakeFiles/project_cpp_exe_helloworld.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project_cpp_exe_helloworld.
+# This may be replaced when dependencies are built.
